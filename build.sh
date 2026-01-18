e8fd652e16c18104d3fec734c9f256e6872ba44e
LuoTainyi-arm64 <2153585992@qq.com>
2026-01-18 15:08:22 +0800

    v1.1.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ver = 1.1.2
+ver = 1.1.3
 py -m build
 pip uninstall printrgb -y
 pip install .\dist\printrgb-${ver}-py3-none-any.whl
--- src/printrgb/__main__.py
+++ src/printrgb/__main__.py
@@ -2,6 +2,8 @@
 import sys
 from . import printrgb
 
+__version__ = "1.1.3"
+
 def main() -> None:
     argv = sys.argv
     ask  = ''
@@ -9,6 +11,8 @@ def main() -> None:
         if len(argv) == 2:
             if argv[1] in ['-h','--help']:
                 ask = 'Argvs of Using printrgb\nprintrgb(*values: object,\nforeground_color: list | tuple | None = None,\nbackground_color: list | tuple | None = None,\nsep: str = " ",\nrainbow: bool = False,\nangle_mode : Literal[\'inner\',\'init\',\'random\'] = \'random\',\nend: str = "\\n",\nfile : object | None = None,\nget_color : types.FunctionType | None = None,\nflush: Literal[False] = False'
+            elif argv[1] in ['-v','--version']:
+                ask = f'printrgb {__version__} by LuoTainyi-arm64'
         printrgb(ask, rainbow = True)
     if not sys.stdin.isatty():
         printrgb(sys.stdin.read(), rainbow = True)
